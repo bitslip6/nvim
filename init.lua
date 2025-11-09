9ab1af8e6a6c9fc7831cfdf37f8c58168777e759
cory marsh <cory@bitfire.co>
2025-11-09 12:44:47 -0700

    latest plugins loaded

--- init.lua
+++ init.lua
@@ -1,12 +1,26 @@
+-- load the lazy plugin manager
 require("config.lazy")
+-- keymappings and vim config
 require("config.digraph")
+require("config.clipboard")
+require("config.keymap")
+require("config.options")
 
+-- color schemes and icons and other UI config
 require("plugins.mini_icons")
-require("plugins.neogit")
+require("plugins.themes")
+require("config.themes")
 
+-- git 
+require("plugins.neogit")
+-- support displaying images in neovim (though kitty, ueberzugpp, opt tmux) 
+require("plugins.image")
+-- fast fuzzy find
+require("plugins.telescope")
+
+-- LSP setup :)
 require("lsp.clangd")
 require("config.clangd")
 
-require("config.keymap")
-
-
+--[[
+]]
