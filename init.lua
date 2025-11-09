8e5adfff9ed6f932fab4f8bdd804c51e07fba000
cory marsh <cory@bitfire.co>
2025-11-09 12:40:11 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("config.lazy")
+require("config.digraph")
+
+require("plugins.mini_icons")
+require("plugins.neogit")
+
+require("lsp.clangd")
+require("config.clangd")
+
+require("config.keymap")
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+basic setup:
+
+store global vim options (tabstop, shift width, leader key, etc) in config/options.lua
